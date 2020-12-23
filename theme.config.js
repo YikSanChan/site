3ef9f83f5d671a06e2af9ef9f094752b20822c44
Yik San Chan <evan.chanyiksan@gmail.com>
2020-12-23 07:59:47 -0800

    trying to add utterances support

--- theme.config.js
+++ theme.config.js
@@ -2,19 +2,35 @@ const YEAR = new Date().getFullYear()
 
 export default {
   footer: (
-    <small style={{ display: "block", marginTop: "8rem" }}>
-      <time>{YEAR}</time> © Yik San Chan.
-      <style
-        dangerouslySetInnerHTML={{
-          __html: `
+    <div>
+      <section id="comments">
+        <h4>
+          <a href="">Comments</a>
+        </h4>
+        <script
+          src="https://utteranc.es/client.js"
+          repo="YikSanChan/yiksanchan.com"
+          issue-term="pathname"
+          label="comments"
+          theme="github-light"
+          crossorigin="anonymous"
+          async
+        />
+      </section>
+      <small style={{ display: "block", marginTop: "8rem" }}>
+        <time>{YEAR}</time> © Yik San Chan.
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `
       @media screen and (max-width: 480px) {
         article { 
           padding-top: 2rem;
           padding-bottom: 4rem;
         }
       }`,
-        }}
-      />
-    </small>
+          }}
+        />
+      </small>
+    </div>
   ),
 }
